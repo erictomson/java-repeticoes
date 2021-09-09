ed6591006ff9b24bf2a2d74296a112ad159653be
erictomson <erictomson@gmail.com>
2021-09-09 10:26:53 -0300

    Exercício: soma dos 20 primeiros inteiros pares e ímpares.

--- src/Repeticoes.java
+++ src/Repeticoes.java
@@ -18,14 +18,34 @@ public class Repeticoes {
 //            i++;
 //        } while(i < 10);
 
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Valor de i: " + i);
+//        for(int i = 0; i < 10; i++) {
+//            System.out.println("Valor de i: " + i);
+//        }
+
+//        for(String mensagem = "valor de i: "; i < 10;) {
+//            System.out.println(mensagem + i);
+//            i++;
+//        }
+
+        int somaPar=0;
+        int somaImpar=0;
+
+        //Soma dos 10 números pares e soma dos 10 números ímpares
+        for(int i=0;i<20;i++) {
+            if(i % 2 == 0) {
+                somaPar = somaPar + i;
+            } else {
+                somaImpar = somaImpar + i;
+            }
         }
 
+        System.out.println("Soma dos pares..: " + somaPar);
+        System.out.println("Soma dos ímpares: " + somaImpar);
+
     }
 
-    private static boolean retornaValorCondicao(int i) {
-        return i < 10;
-    }
+//    private static boolean retornaValorCondicao(int i) {
+//        return i < 10;
+//    }
 
 }
